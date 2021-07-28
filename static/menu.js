0f00a463d1f3111c465c1ec3286ebc086c94951b
Ezonater <itzstam@gmail.com>
2021-07-28 18:31:51 -0400

    about us

---
+++ static/menu.js
@@ -0,0 +1,8 @@
+function hamburger(){
+    if(document.getElementById("hamburger-menu").style.width != "0%") document.getElementById("hamburger-menu").style.width = "0%";
+    else document.getElementById("hamburger-menu").style.width = "20%";
+}
+
+function aboutUs(){
+    location.href = "aboutus.html"
+}
